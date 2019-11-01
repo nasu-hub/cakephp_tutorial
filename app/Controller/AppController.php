1b5e909c5cc1f336775dca7fffef67cffbd496cf
nasu-hub <y_nasu@funteam.co.jp>
2019-11-01 01:45:58 +0000

    ブログチュートリアル

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,36 @@ App::uses('Controller', 'Controller');
  * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public $components = array(
+        'Flash',
+        'Auth' => array(
+            'loginRedirect' => array(
+                'controller' => 'posts',
+                'action' => 'index'
+            ),
+            'logoutRedirect' => array(
+                'controller' => 'pages',
+                'action' => 'display',
+                'home'
+            ),
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish'
+                )
+            ),
+            'authorize' => array('Controller')
+        )
+    );
+
+    public function beforeFilter(){
+        $this->Auth->allow('index', 'view');
+    }
+
+    public function isAuthorized($user) {
+        if (isset($user['role']) && $user['role'] === 'admin') {
+            return true;
+        }
+        return false;
+    }
 }
